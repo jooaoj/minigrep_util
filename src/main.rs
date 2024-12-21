497c3942a8eb831b5f9895b5d21cc8407c14dbfc
Jooa Jaakkola <jaakkola.jooa@gmail.coma>
2024-12-21 14:28:50 +0200

    Basic functionality: accept args and stdout

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
-use std::env;
+use std::{ env, fs };
+
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+    //dbg!(&args);
+    let query = &args[1];
+    let path = &args[2];
+    let content = fs::read_to_string(path).expect("File does not exist");
+
+    println!("Searching for {query}...");
+    println!("In file:\n´´´{path}\n{content}\n´´´");
 }
