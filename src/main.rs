bae859dc09c7789943063e8eac527a5c916e4e81
Jooa Jaakkola <jaakkola.jooa@gmail.coma>
2025-01-15 22:53:08 +0200

    standard errpr correctly

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     //dbg!(&args);
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
@@ -16,7 +16,7 @@ fn main() {
 
     // Error from main
     if let Err(e) = minigrep_util::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
